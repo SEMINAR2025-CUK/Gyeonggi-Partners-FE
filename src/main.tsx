4f397459c1777bf1a55be0493a43735c583feea4
stevehon12 <stevehong124@gmail.com>
2025-10-23 13:22:24 +0900

    add : krds 설치
    -krds npm 설치
    -krds 전역설정

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@
   import { createRoot } from "react-dom/client";
   import App from "./App.tsx";
   import "./index.css";
+  import "krds-uiux/resources/cdn/krds.min.css";
+  import "krds-uiux/resources/cdn/krds.min.js";
+  //css,js 외 필요시 임포트 해서 사용 가능
+
 
   createRoot(document.getElementById("root")!).render(<App />);
   
